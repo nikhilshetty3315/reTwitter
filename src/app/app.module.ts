68a68aaba3c7e167dcbab5d6c8964e838220f3e2
nikhilshetty3315 <nikhil.bilakanti@gmail.com>
2019-08-10 18:30:28 +0530

    working source code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { FeedpageComponent } from './feedpage/feedpage.component';
-=======
-import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> fdb7574ecf215b85f75b0e1deb8b340d673ec6b7
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-    FeedpageComponent
-=======
-    NavbarComponent
->>>>>>> fdb7574ecf215b85f75b0e1deb8b340d673ec6b7
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
