bacce36d680ce9188ca8f086c9c1864d0acbc109
nikhilshetty3315 <nikhil.bilakanti@gmail.com>
2019-08-12 14:26:56 +0530

    error page successfully updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FeedpageComponent } from './feedpage/feedpage.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome', component: WelcomeComponent }, 
-  { path: 'feedpage', component: FeedpageComponent }
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'feedpage', component: FeedpageComponent },
+  { path: '**', component: ErrorpageComponent }
 ];
 
 @NgModule({
