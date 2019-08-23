96e2cec6d8cfa6afc21533b1cf2d81e3e2385ac0
abhithjohnanto-k <abhithjohnanto@gmail.com>
2019-08-23 12:43:02 +0530

    no conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,15 @@ import { FeedpageComponent } from './feedpage/feedpage.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { LogoutComponent } from './logout/logout.component';
-<<<<<<< HEAD
+import { RouteGuardService } from './service/route-guard.service';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  {path: 'signup', component: SignupComponent},
-  { path: 'feedpage', component: FeedpageComponent },
-  { path: 'welcome', component: WelcomeComponent },
-=======
-import { RouteGuardService } from './service/route-guard.service';
-
-const routes: Routes = [
-  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
   { path: 'feedpage', component: FeedpageComponent, canActivate: [RouteGuardService]},
   { path: 'welcome', component: WelcomeComponent, canActivate: [RouteGuardService] },
->>>>>>> 5e0f186affa49ef04d054bf39dcc141631dc4134
   { path: 'logout', component: LogoutComponent},
   { path: '**', component: ErrorpageComponent }
 ];
