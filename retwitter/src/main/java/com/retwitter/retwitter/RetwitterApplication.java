4ffdba08b4121b921dc8525a409407c334db011b
abhithjohnanto-k <abhithjohnanto@gmail.com>
2019-08-23 12:30:36 +0530

    change 2

---
+++ retwitter/src/main/java/com/retwitter/retwitter/RetwitterApplication.java
@@ -0,0 +1,13 @@
+package com.retwitter.retwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetwitterApplication.class, args);
+	}
+
+}
--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+              private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(['login']);
+
+    return false;
+  }
 }
